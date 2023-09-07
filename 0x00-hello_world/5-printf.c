781d0f840e3d15918099972605fd7bdf5612c9ee
maitha1-nule <nuleahmed@gmail.com>
2023-09-07 15:07:01 -0700

    My first printf program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - this is the main function in the program
+ * Return: 0 always
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
