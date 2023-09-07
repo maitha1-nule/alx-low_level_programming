9cccb399edb6e5c55f6fd02cb72cc26905dcc5d3
maitha1-nule <nuleahmed@gmail.com>
2023-09-07 14:55:03 -0700

    my first program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - this is the main part of the program
+ * Return:0 always
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
