fe881ef61b3709fa07db0ad53cd7e8c7795b56f4
maitha1-nule <nuleahmed@gmail.com>
2023-09-08 15:18:59 -0700

    putchar alternative to print usage

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - this is the main function of the program
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		putchar('\n');
+	}
+	return (0);
+}
