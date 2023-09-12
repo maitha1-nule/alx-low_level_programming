62d1d5d7201e951a15e723bc4c30e97f83a6a99d
maitha1-nule <nuleahmed@gmail.com>
2023-09-12 15:36:50 -0700

    A program that prints the signs of inputs entered

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - this is function prototype
+ * Description: checks whether a no is positive or negative
+ * @n: this is the argument used
+ *
+ * Return: 1 and print + if true else 0 - and -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else
+		if (n == 0)
+		{
+			_putchar(45);
+			return (0);
+		}
+		else
+			{
+				_putchar(48);
+				return (-1);
+			}
+}
