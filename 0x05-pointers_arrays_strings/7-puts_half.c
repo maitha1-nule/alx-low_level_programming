740dbef13dd80140013244297396f6ffca599f00
maitha1-nule <nuleahmed@gmail.com>
2023-09-18 19:45:28 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - this is the function name
+ * @str: is the argument
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	count++;
+	n = (count - 1) / 2;
+	for (i = n + 1; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
