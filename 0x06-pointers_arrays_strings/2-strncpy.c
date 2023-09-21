906b5da0f9ebb159bed9abc8a4db9cf5c6645c6e
maitha1-nule <nuleahmed@gmail.com>
2023-09-21 15:35:01 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h'
+
+/**
+* _strncpy - the main function of the code
+* @src: parameter
+* @dest: parameter
+* @n:  paramter
+*
+* Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int k;
+
+	k = 0;
+
+	while (k < n && src[k] != '\0')
+	{
+		dest[k] = src[k];
+		k++;
+	}
+	while (k < n)
+	{
+		dest[k] = '\0';
+		k++;
+	}
+	return (dest);
+}
