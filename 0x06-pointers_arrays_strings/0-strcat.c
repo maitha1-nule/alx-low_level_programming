9eee54ed713d7535fed7c2d8c58273a5b448583c
maitha1-nule <nuleahmed@gmail.com>
2023-09-21 14:46:21 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strcat - main function prototype
+ *
+ * @src: concactinate parameter
+ *
+ * @desk: contactinate parameter
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int k, r;
+
+k = 0;
+r = 0;
+
+while (dest[k] != '\0')
+{
+	k++;
+}
+
+while (src[r] != '\0')
+{
+	dest[k] = src[r];
+	k++;
+	r++;
+}
+dest[k] = '\0';
+return (dest);
+}
