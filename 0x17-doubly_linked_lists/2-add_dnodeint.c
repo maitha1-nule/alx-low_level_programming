9609ca9e96721e1741a65d0e48f9439785e1fbd4
maitha1-nule <nuleahmed6@gmail.com>
2023-12-08 02:38:57 +0300

    this task was all about writting a function that adds a new node at the beginning of a dlistint_t list.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,8 +17,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		temp->n = n;
 		temp->next = NULL;
 		temp->next = *head;
-	if(*head != NULL)
-		(*head)->prev = temp;
+		head->prev = temp;
 		*head = temp;
-		return temp;
+		return *head;
 }
