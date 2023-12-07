0c819cd3104a31870b119bb8b1b6c5890acf3ea2
maitha1-nule <nuleahmed6@gmail.com>
2023-12-08 01:03:15 +0300

    In this first taski created a program which prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-        size_t nodes = 0;
-        while (h != NULL)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-                nodes++;
-        }
-        return (nodes);
+	size_t nodes = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
 }
